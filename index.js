6b4bad541e83e7120436a199ddf2f4eb7327c20b
surenjun <tlsurenjun@163.com>
2019-08-04 18:31:13 +0800

    new file:   .gitignore 	new file:   README.md 	new file:   index.js 	new file:   package.json 	new file:   src/assets/test-data.js 	new file:   src/index.js 	new file:   src/taro-index.js 	new file:   src/template.xtpl 	new file:   test/index.js 	new file:   yarn-error.log 	new file:   yarn.lock

---
+++ index.js
@@ -0,0 +1,29 @@
+const {resolve, join ,basename} = require("path");
+const fs = require("fs");
+const prettier = require('prettier');
+const dslHelper = require('@imgcook/dsl-helper');
+const _ = require('lodash');
+
+const codeData = require("./src/assets/test-data");    //json数据文件
+const switchFn = require('./src/taro-index');          //转换函数
+
+(async () => {
+  const baseName = basename(resolve(__dirname, './src/assets/test-data'));
+
+  const renderInfo = switchFn(codeData, {
+    prettier,
+    _,
+    helper: dslHelper,
+  },baseName);
+
+  /*const ret = xtplRender(
+    resolve(__dirname, '../src/template.xtpl'),
+    renderInfo,
+    {}
+  );*/
+
+  const {tsx, less} = renderInfo.renderData;
+  fs.mkdirSync(resolve(__dirname, './dist'));
+  await fs.writeFile(join(__dirname, `./dist/${baseName}.tsx`), tsx, () => console.log(`${baseName}.tsx文件生成完成`));
+  await fs.writeFile(join(__dirname, `./dist/${baseName}.less`), less, () => console.log(`${baseName}.less文件生成完成`));
+})();
